96effcc43670226d9dfb1073cdd9dd28a1b634c4
freak_ipr <143888307+NoSpaceAvailable@users.noreply.github.com>
2023-12-24 19:09:32 +0700

    Fix bugs in server.c

--- server.c
+++ server.c
@@ -10,7 +10,7 @@
 
 int main(){
 	/* Open an IPV4, send and receive socket on machine */
-	/* It returns socket descriptor or -1 on failure, so I save it in variable 's' to manage */
+	/* It returns a socket descriptor or -1 on failure, so I save it in variable 's' to manage */
 	printf("Creating server endpoint (socket) ...\n");
 	int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
 	/* Error handling */
@@ -22,7 +22,7 @@ int main(){
 		printf("Socket created.\n");
 	}
 
-	/* Inittalize a sockaddr_in object to store data stream */
+	/* Initialize a sockaddr_in object to store data stream */
 	const struct sockaddr_in addr = {
 		.sin_family = AF_INET, /* Refers to IPV4 protocol */
 		.sin_port = htons(8080), /* Refers to 8080. Port 8080 is 0x1f90 in hex,
@@ -30,7 +30,7 @@ int main(){
 		.sin_addr = 0 /* Use default setting */
 	};
 
-	/* Once socket opened successfully, bind it with a port */
+	/* Once the socket opened successfully, bind it with a port */
 	/* It returns 0 on success and -1 on failure */
 	printf("Binding port ...\n");
 	int b = bind(socket_fd, &addr, sizeof(addr));
@@ -42,7 +42,7 @@ int main(){
 		printf("Port binded.\n");
 	}
 
-	/* Once port binded successfully, listen to incoming connections */
+	/* Once the port binded successfully, listen for incoming connections */
 	/* On success, 0 is returned. Otherwise, return -1 */
 	printf("Initialize listening channel ...\n");
 	int l = listen(socket_fd, 10); // 10 connections at a time is enough for POC
@@ -61,21 +61,21 @@ int main(){
 	   -	int sockfd: socket file descriptor of opening socket on the server.
 			    This socket is initialized with socket() function, bind to a specific port with bind()
 			    and ready to listen with listen().
-	   -	struct sockaddr *addr: a pointer to the socket which belong to the incoming connection (a.k.a client).
-				       If 0 is assigned, it means that the server will accept the connection but don't know
-				       any information about it, like IP address. If you want to collect information,
-				       you have to init a sockaddr object for client and pass the pointer as parameter.
-				       After that, you can extract infor from sockaddr's properties.
+	   -	struct sockaddr *addr: a pointer to the socket which belongs to the incoming connection (a.k.a client).
+				       If 0 is assigned, it means that the server will accept the connection but doesn't know
+				       any information about it, like the IP address. If you want to collect information,
+				       you have to init a sockaddr object for the client and pass the pointer as a parameter.
+				       After that, you can extract info from sockaddr's properties.
 	   -	socklen_t *addrlen: this is a value-result argument, which is a declared term: "Value-result argument is an
-				    argument that its value can be changed when being passed to a function". You can initialize
+				    argument that its value can be changed when passed to a function". You can initialize
 				    it like this: socklen_t sockSize = sizeof(client_socket). This argument is used to tell the
-				    accept() function the actual size of client socket address. addrlen's value and the actual size
-				    of client socket address can be different, potentially result to data loss when transfering data.
-				    So, it's neccessary to pass a pointer *addrlen instead of only pass its value. The accept() function
+				    accept() function the actual size of the client socket address. addrlen's value and the actual size
+				    of the client socket address can be different, potentially resulting in data loss when transferring data.
+				    So, it's necessary to pass a pointer *addrlen instead of only passing its value. The accept() function
 				    can compare the actual size of sockaddr with addrlen and update addrlen value if those two are
 				    different, avoid losing data.
 
-		If sockaddr is NULL, it means the server only accept connection from clients but don't need to know anything about it.
+		If sockaddr is NULL, the server only accepts connections from clients but doesn't need to know anything about it.
 		In this situation, addrlen also left NULL */
 	int client_fd = accept(socket_fd, 0, 0);
 	if (client_fd < 0){
@@ -85,11 +85,11 @@ int main(){
 	else{
 		printf("Request accepted!\n");
 	}
-
-	/* When request is accepted, receive data from it */
-	/* Return -1 when error(s) occur, otherwise return the number of byte received. When end-of-file reached, return 0 */
-	/* Only use this function with connected socket */
-	char data_buffer[256]; // Buffer to store received data
+	
+	char data_buffer[256]; // Buffer to store received request.
+	/* When a request is accepted, receive data from it */
+	/* Return -1 when error(s) occur. Otherwise, return the number of bytes received. When end-of-file is reached, return 0 */
+	/* Only use this function with a connected socket */
 	int rec = recv(client_fd, data_buffer, 256, 0);
 	if (rec < 0){
 		printf("Bad data. Exiting ...\n");
@@ -119,11 +119,11 @@ int main(){
 		printf("File opened.\n");
 	}
 
-	/* Transfer data from client's file descriptor to server's file descriptor */
+	/* Transfer data from the client's file descriptor to the server's file descriptor */
 	while (true){
-		int sent = sendfile(client_fd, server_fd, NULL, 256); /* client_fd is the file to writem server_fd is the file to read,
+		int sent = sendfile(client_fd, server_fd, NULL, 256); /* client_fd is the file to write server_fd is the file to read,
 								 	 NULL pass to off_t *offset (a pointer to the position that this
-								 	 function should read in server_fd) means just read from the start of file.*/
+								 	 function should read in server_fd) means just read from the start of the file.*/
 		if (sent < 0){
 			printf("Cannot send file due to unknown error(s). Exiting ...\n");
 			return 0;
